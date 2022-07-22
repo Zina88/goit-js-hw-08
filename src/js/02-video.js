e496221d5ff09d7faab9f3f05525698ee505384a
Zina <zinasaenko@hotmail.com>
2022-07-22 18:42:39 +0300

    edits

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,15 @@
-// const Vimeo = require('vimeo').Vimeo;
-// В HTML есть <iframe> с видео для Vimeo плеера. Напиши скрипт который будет сохранять текущее время воспроизведения видео в локальное хранилище и, при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const throttle = require('lodash.throttle');
 
-//     <iframe
-//       id="vimeo-player"
-//       src="https://player.vimeo.com/video/236203659"
-//       width="640"
-//       height="360"
-//       frameborder="0"
-//       allowfullscreen
-//       allow="autoplay; encrypted-media"
-//     ></iframe>
+// player.on('timeupdate', function (data) {
+//   console.log(data.seconds);
+// });
 
-// Выполняй это задание в файлах 02-video.html и 02-video.js. Разбей его на несколько подзадач:
+player.on('timeupdate', throttle(onPlay, 1000));
 
-// Ознакомься с документацией библиотеки Vimeo плеера.
-// Добавь библиотеку как зависимость проекта через npm.
-// Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
