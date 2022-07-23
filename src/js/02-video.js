1af94308534b1270769520cb6a5e4e3682c576db
Zina <zinasaenko@hotmail.com>
2022-07-23 20:03:11 +0300

    edits

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,12 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 }
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', onFormInput);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+updateOutput();
+
+function onSubmitForm(e) {
+  e.preventDefault(); //отменяем перезагрузку стр
+  e.currentTarget.reset(); //очищаем форму
+  localStorage.removeItem(LOCALSTORAGE_KEY); //очищаем форму, если пол-ль нажал submit
+}
+
+function onFormInput(e) {
+  const message = e.currentTarget.value; //записываем значение value в message
+  //   console.log(message);
+  localStorage.setItem(LOCALSTORAGE_KEY, message); //записываем в лок хранилище message под ключом feedback-msg
+}
+function updateOutput() {
+  const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedMessage) {
+    //если savedMessage === true => записываем новое значение в refs.textarea.value
+    console.log(savedMessage);
+    form.value = savedMessage;
+  } //иначе => ничего не делаем
+}
