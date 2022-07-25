b39aab6e031d3a67456e3d7094c9c18284368d5a
Zina <zinasaenko@hotmail.com>
2022-07-25 22:05:02 +0300

    edits

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,28 +9,27 @@ const message = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const formMessage = {};
 
-updateInput();
+updateForm();
 
 function onSubmitForm(e) {
-  e.preventDefault(); //отменяем перезагрузку стр
-  e.currentTarget.reset(); //очищаем форму
-  localStorage.removeItem(LOCALSTORAGE_KEY); //очищаем форму, если пол-ль нажал submit
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
 function onFormInput(e) {
-  formMessage[e.target.name] = e.target.value; //записываем значение value в formMessage
-  //   console.log(message);
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formMessage)); //записываем в лок хранилище message под ключом feedback-msg
+  formMessage[e.target.name] = e.target.value;
+  // console.log(formMessage[e.target.name]);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formMessage));
 }
 
-function updateInput() {
+function updateForm() {
   const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
   if (savedMessage) {
-    //если savedMessage === true => записываем новое значение
     email.value = savedMessage.email;
-    console.log(email.value);
+    // console.log(email.value);
     message.value = savedMessage.message;
-    console.log(message.value);
-  } //иначе => ничего не делаем
+    // console.log(message.value);
+  }
 }
