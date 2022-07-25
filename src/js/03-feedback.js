8e580e4d5b58b39e7b60cc61b59f3e9809d0e04f
Zina <zinasaenko@hotmail.com>
2022-07-25 20:59:09 +0300

    edits

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,15 @@
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+
 form.addEventListener('submit', onSubmitForm);
-form.addEventListener('input', onFormInput);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formMessage = {};
 
 updateOutput();
 
@@ -12,18 +17,24 @@ function onSubmitForm(e) {
   e.preventDefault(); //отменяем перезагрузку стр
   e.currentTarget.reset(); //очищаем форму
   localStorage.removeItem(LOCALSTORAGE_KEY); //очищаем форму, если пол-ль нажал submit
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
 }
 
 function onFormInput(e) {
-  const message = e.currentTarget.value; //записываем значение value в message
+  formMessage[e.target.name] = e.target.value; //записываем значение value в message
   //   console.log(message);
-  localStorage.setItem(LOCALSTORAGE_KEY, message); //записываем в лок хранилище message под ключом feedback-msg
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formMessage)); //записываем в лок хранилище message под ключом feedback-msg
 }
+
 function updateOutput() {
-  const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+  const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
   if (savedMessage) {
-    //если savedMessage === true => записываем новое значение в refs.textarea.value
-    console.log(savedMessage);
-    form.value = savedMessage;
+    //если savedMessage === true => записываем новое значение
+
+    email.value = savedMessage.email;
+    console.log(email.value);
+    message.value = savedMessage.message;
+    console.log(message.value);
   } //иначе => ничего не делаем
 }
